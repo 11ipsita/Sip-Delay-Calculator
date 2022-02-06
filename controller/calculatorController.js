7d4377ab7845312e0dde6f4cdc81fe5b0d6262af
11ipsita <61733847+11ipsita@users.noreply.github.com>
2022-02-06 16:52:45 +0530

    Update calculatorController.js

--- controller/calculatorController.js
+++ controller/calculatorController.js
@@ -16,9 +16,12 @@ const Calculatecontrol = (req,res)=>{
         var info = servicelogic(monthlyinvestment,investmentperiod,rateofreturn,delay);
      
     //Result string...    
-    res.send(` Delay of ${delay} months in starting your SIP will cause a national Loss of: Rs.${info.loss} in the final value of your investment. 
+    /*res.send(` Delay of ${delay} months in starting your SIP will cause a national Loss of: Rs.${info.loss} in the final value of your investment. 
       If you start investing from today then the final value is Rs.${info.starttoday}.
-      If you start investing after ${delay} months then the final value is Rs.${info.delayedstart}.` ); 
+      If you start investing after ${delay} months then the final value is Rs.${info.delayedstart}.` ); */
+
+      var finalresult = `If you start your investment from today,the final value will be ${info.startfromtoday}.\nIf you start your investment after ${delay} months,the final value will be ${info.delayedstart}.\nAnd it will cause notional loss of :${info.lossfromdelay} in the final value of your investment.`
+      res.send(finalresult);
     }    
 }
 
