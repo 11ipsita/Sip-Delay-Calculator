50a5e2b3c775f6c0a2ac60fc3cf5822ee38cb8d4
11ipsita <61733847+11ipsita@users.noreply.github.com>
2022-02-06 16:37:10 +0530

    Update calculatorController.js

--- controller/calculatorController.js
+++ controller/calculatorController.js
@@ -1,1 +1,25 @@
+const servicelogic=require('../services/bussinesslogic');
 
+
+const Calculatecontrol = (req,res)=>{
+    const{monthlyinvestment,investmentperiod,rateofreturn,delay}=req.body;
+    //Validations for user input...
+    if(!(monthlyinvestment >= 0)){
+        res.send("Invalid monthlyinvestment");
+    }else if(!(investmentperiod > 0)){
+        res.send("Invalid investmentperiod");
+    }else if(!(rateofreturn > 0)){
+        res.send("Invalid rateofreturn");
+    }else if(!(delay > 0)){
+        res.send("Invalid delay");
+    }else {
+        var info = servicelogic(monthlyinvestment,investmentperiod,rateofreturn,delay);
+     
+    //Result string...    
+    res.send(` Delay of ${delay} months in starting your SIP will cause a national Loss of: Rs.${info.loss} in the final value of your investment. 
+      If you start investing from today then the final value is Rs.${info.starttoday}.
+      If you start investing after ${delay} months then the final value is Rs.${info.delayedstart}.` ); 
+    }    
+}
+
+module.exports= Calculatecontrol;
